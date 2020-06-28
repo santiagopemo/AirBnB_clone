8545824af693cdf7a7857bee33d60b8b6538b202
dondropo <alejandroruscamoreno@gmail.com>
2020-06-28 20:01:45 +0000

    user class

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+user class
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """class user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
