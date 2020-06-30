3b8d95a465a7636a71c015a5f6edbee81bbfb2ec
santiagopemo <santiagopemo@gmail.com>
2020-06-30 15:27:49 +0000

    corrected test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,7 +8,11 @@ import models
 import inspect
 import os
 from datetime import datetime
-BaseModel = models.base_model.BaseModel
+CurrentTestClass = models.base_model.BaseModel
+current_module = models.base_model
+file_name = os.path.basename(__file__)
+current_path = "tests/test_models/{}".format(file_name)
+test_path = "models/{}".format(file_name[5:])
 
 
 class TestBaseModel(unittest.TestCase):
@@ -24,83 +28,70 @@ class TestBaseModel(unittest.TestCase):
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(
                                        [
-                                        'models/base_model.py',
-                                        'tests/test_models/test_base_model.py'
+                                        test_path,
+                                        current_path
                                        ]
                                       )
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_docstring(self):
-        self.assertTrue(len(models.base_model.__doc__) >= 1)
-        self.assertTrue(len(BaseModel.__doc__) >= 1)
+        self.assertTrue(len(current_module.__doc__) >= 1)
+        self.assertTrue(len(CurrentTestClass.__doc__) >= 1)
         methods = inspect.getmembers(
-                                     BaseModel,
+                                     CurrentTestClass,
                                      inspect.isfunction
                                     )
         for m in methods:
             self.assertTrue(len(m[1].__doc__) >= 1)
 
     def test_base_model_single_instance(self):
-        bm1 = BaseModel()
-        self.assertTrue(hasattr(bm1, 'id'))
-        self.assertTrue(hasattr(bm1, 'created_at'))
-        self.assertTrue(hasattr(bm1, 'updated_at'))
-        self.assertEqual(type(bm1.id), str)
-        self.assertEqual(type(bm1.created_at), datetime)
-        self.assertEqual(type(bm1.updated_at), datetime)
-        bm_funcs = inspect.getmembers(bm1)
+        cti1 = CurrentTestClass()
+        self.assertTrue(hasattr(cti1, 'id'))
+        self.assertTrue(hasattr(cti1, 'created_at'))
+        self.assertTrue(hasattr(cti1, 'updated_at'))
+        self.assertEqual(type(cti1.id), str)
+        self.assertEqual(type(cti1.created_at), datetime)
+        self.assertEqual(type(cti1.updated_at), datetime)
+        bm_funcs = inspect.getmembers(cti1)
         func_names = [fn[0] for fn in bm_funcs]
         self.assertIn('save', func_names)
         self.assertIn('to_dict', func_names)
-        # bm1_dict = {
-        #             '__class__': 'BaseModel',
-        #             'id': bm1.id,
-        #             'created_at': bm1.created_at.isoformat(),
-        #             'updated_at': bm1.updated_at.isoformat()
-        #            }
-        # self.assertDictEqual(bm1.to_dict(), bm1_dict)
-        # old_date = bm1.updated_at
-        # bm1.save()
-        # self.assertNotEqual(old_date, bm1.updated_at)
-        # self.assertLess(old_date, bm1.updated_at)
-        # self.assertNotEqual(bm1.created_at, bm1.updated_at)
-        # self.assertLess(bm1.created_at, bm1.updated_at)
 
     def test_two_instances(self):
-        bm1 = BaseModel()
-        bm2 = BaseModel()
-        self.assertNotEqual(bm1.id, bm2.id)
-        self.assertNotEqual(bm1.created_at, bm2.created_at)
-        self.assertLess(bm1.created_at, bm2.created_at)
-        self.assertNotEqual(bm1.updated_at, bm2.updated_at)
-        self.assertLess(bm1.updated_at, bm2.updated_at)
+        cti1 = CurrentTestClass()
+        cti2 = CurrentTestClass()
+        self.assertNotEqual(cti1.id, cti2.id)
+        self.assertNotEqual(cti1.created_at, cti2.created_at)
+        self.assertLess(cti1.created_at, cti2.created_at)
+        self.assertNotEqual(cti1.updated_at, cti2.updated_at)
+        self.assertLess(cti1.updated_at, cti2.updated_at)
 
     def test_str_method(self):
-        bm1 = BaseModel()
-        bm1.id = "1234"
-        bm1.name = "Holberton"
-        bm1.my_number = 89
-        bm1_dict = {
-                    'id': bm1.id,
-                    'created_at': repr(bm1.created_at),
-                    'updated_at': repr(bm1.updated_at),
+        cti1 = CurrentTestClass()
+        cti1.id = "1234"
+        cti1.name = "Holberton"
+        cti1.my_number = 89
+        cti1_dict = {
+                    'id': cti1.id,
+                    'created_at': repr(cti1.created_at),
+                    'updated_at': repr(cti1.updated_at),
                     'name': 'Holberton',
                     'my_number': 89,
                    }
-        cls_id = "[BaseModel] (1234)"
-        self.assertIn(cls_id, bm1.__str__())
-        for k, v in bm1_dict.items():
-            self.assertIn(str(k), bm1.__str__())
-            self.assertIn(str(v), bm1.__str__())
+        cls_id = "[{}] (1234)".format(cti1.__class__.__name__)
+        self.assertIn(cls_id, cti1.__str__())
+        for k, v in cti1_dict.items():
+            self.assertIn(str(k), cti1.__str__())
+            self.assertIn(str(v), cti1.__str__())
 
     def test_with_kwarg(self):
         date = datetime.now().isoformat()
-        bm1 = BaseModel(id='12345', created_at=date, updated_at=date)
+        cti1 = CurrentTestClass(id='12345', created_at=date, updated_at=date)
         date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
-        self.assertEqual(bm1.id, "12345")
-        self.assertEqual(str(bm1.created_at), str(date))
-        self.assertEqual(str(bm1.updated_at), str(date))
+        self.assertEqual(cti1.id, "12345")
+        self.assertEqual(str(cti1.created_at), str(date))
+        self.assertEqual(str(cti1.updated_at), str(date))
 
 
 if __name__ == "__main__":
