08c3304351bebe673a05f6d24c6a713a4a39e2d2
santiagopemo <santiagopemo@gmail.com>
2020-07-01 01:37:45 +0000

    added docstring documentation

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -28,9 +28,15 @@ class TestFileStorage(unittest.TestCase):
     """TestFileStorage class"""
 
     def setUp(self):
-        pass
+        """Executes before each test"""
+        FileStorage._FileStorage__objects = {}
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
 
     def tearDown(self):
+        """Executes after each test"""
         FileStorage._FileStorage__objects = {}
         try:
             os.remove("file.json")
@@ -38,6 +44,7 @@ class TestFileStorage(unittest.TestCase):
             pass
 
     def test_pep8(self):
+        """Check PEP8 style"""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(
                                        [
@@ -49,6 +56,7 @@ class TestFileStorage(unittest.TestCase):
                          "Found code style errors (and warnings).")
 
     def test_docstring(self):
+        """Checks the docstring documentation"""
         self.assertTrue(len(current_module.__doc__) >= 1)
         self.assertTrue(len(FileStorage.__doc__) >= 1)
         methods = inspect.getmembers(
@@ -59,6 +67,7 @@ class TestFileStorage(unittest.TestCase):
             self.assertTrue(len(m[1].__doc__) >= 1)
 
     def test_file_storage_instance(self):
+        """Test FileStorage instance"""
         fs = FileStorage()
         self.assertTrue(hasattr(fs, '_FileStorage__file_path'))
         self.assertTrue(hasattr(fs, '_FileStorage__objects'))
@@ -72,12 +81,14 @@ class TestFileStorage(unittest.TestCase):
         self.assertIn('reload', func_names)
 
     def test_instance_with_args(self):
+        """Test Filestorage instance with args"""
         with self.assertRaises(TypeError):
             sf = FileStorage(1)
         with self.assertRaises(TypeError):
             sf = FileStorage(None)
 
     def test_all(self):
+        """Test all fucntion"""
         fs = FileStorage()
         objs = fs.all()
         self.assertEqual(len(objs), 0)
@@ -98,6 +109,7 @@ class TestFileStorage(unittest.TestCase):
             self.assertIs(o, objs[key])
 
     def test_all_args(self):
+        """Test all function with args"""
         fs = FileStorage()
         with self.assertRaises(TypeError):
             fs.all(1)
@@ -105,6 +117,7 @@ class TestFileStorage(unittest.TestCase):
             fs.all(None)
 
     def test_new(self):
+        """Test new function"""
         fs = FileStorage()
         bm = BaseModel()
         am = Amenity()
@@ -121,11 +134,13 @@ class TestFileStorage(unittest.TestCase):
             self.assertIs(o, fs.all()[key])
 
     def test_new_no_args(self):
+        """Test new function with no arguments"""
         fs = FileStorage()
         with self.assertRaises(TypeError):
             fs.new()
 
     def test_new_two_args(self):
+        """Test new function with two arguments"""
         fs = FileStorage()
         bm = BaseModel()
         pl = Place()
@@ -135,6 +150,7 @@ class TestFileStorage(unittest.TestCase):
             fs.new(bm, pl)
 
     def test_new_wrong_args(self):
+        """Test new function with wrong arguments"""
         fs = FileStorage()
         with self.assertRaises(AttributeError):
             fs.new(3)
@@ -144,6 +160,7 @@ class TestFileStorage(unittest.TestCase):
             fs.new("BaseModel")
 
     def test_save(self):
+        """Test save function"""
         fs = FileStorage()
         bm = BaseModel()
         am = Amenity()
@@ -162,6 +179,7 @@ class TestFileStorage(unittest.TestCase):
             self.assertIn(key, objs)
 
     def test_save_args(self):
+        """Test save function with arguments"""
         fs = FileStorage()
         with self.assertRaises(TypeError):
             fs.save(None)
@@ -169,6 +187,7 @@ class TestFileStorage(unittest.TestCase):
             fs.save(2)
 
     def test_reload(self):
+        """Test reload function"""
         fs = FileStorage()
         bm = BaseModel()
         am = Amenity()
@@ -190,6 +209,7 @@ class TestFileStorage(unittest.TestCase):
             self.assertIn(key, objs)
 
     def test_reload_args(self):
+        """Test reload function with arguments"""
         fs = FileStorage()
         with self.assertRaises(TypeError):
             fs.reload(None)
